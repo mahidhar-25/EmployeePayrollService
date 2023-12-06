bed7a7937d337519c67d97c3e452e6d4f5a11416
mahidhar reddy <mahidhar100008@gmail.com>
2023-12-06 06:45:18 +0530

    Added : intialized employee payroll service

---
+++ src/com/bridgelabz/employeepayroll/Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!! welcome to employee pay roll service !!!");
+    }
+}
